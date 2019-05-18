dfab08d5b756b8fe4e94079e9822ba3f3dbf6f85
RogerRatu <rodgaratu@windowslive.com>
2019-05-18 15:00:22 +1200

    Commit #2

--- Assignment2PartD/src/PartD.java
+++ Assignment2PartD/src/PartD.java
@@ -3,8 +3,9 @@ public class PartD {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hi, my name is_____");
-
+		System.out.println("Hi, my name is_____"); 
+		
+		System.out.println("My favourite colour is _____");
 	}
 
 }
